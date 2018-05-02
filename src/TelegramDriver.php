cba2774c81f5aafa6c7590696c817ea285848d16
Maxim <m@iqcompany.ru>
2018-05-02 10:47:48 +0300

    Update TelegramDriver.php (#42)
    Fixed issue #41.

--- src/TelegramDriver.php
+++ src/TelegramDriver.php
@@ -40,7 +40,12 @@ class TelegramDriver extends HttpDriver
     public function buildPayload(Request $request)
     {
         $this->payload = new ParameterBag((array) json_decode($request->getContent(), true));
-        $this->event = Collection::make($this->payload->get('message'));
+
+        $message = $this->payload->get('message');
+        if (empty($message)) {
+            $message = $this->payload->get('edited_message');
+        }
+        $this->event = Collection::make($message);
         $this->config = Collection::make($this->config->get('telegram'));
         $this->queryParameters = Collection::make($request->query);
     }
